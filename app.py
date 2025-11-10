163b9d6717b4d260317cb8f901024d09de3cdb22
hiro564 <120894368+hiro564@users.noreply.github.com>
2025-11-10 15:29:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -468,22 +468,7 @@ def detect_and_build_graph(binary_img, curvature_threshold, max_jump, min_transi
             else:
                 break
     
-    # ノードを描画
-    for node_id, data in nodes.items():
-        x, y = data['pos']
-        if data['type'] == 0:
-            color = (255, 0, 0)
-        elif data['type'] == 1:
-            color = (0, 0, 255)
-        elif data['type'] == 2:
-            color = (0, 255, 255)
-        elif data['type'] == 3:
-            color = (0, 165, 255)
-        
-        radius = 5 if data['type'] != 3 else 3
-        cv2.circle(marked_img, (x, y), radius, color, -1)
-        
-    # --- 追加: 小さい孤立ネットワーク（ノード数3以下）を削除 ---
+     # --- 追加: 小さい孤立ネットワーク（ノード数3以下）を削除 ---
     def connected_components(graph_dict):
         visited = set()
         components = []
@@ -505,6 +490,21 @@ def detect_and_build_graph(binary_img, curvature_threshold, max_jump, min_transi
         if len(comp) <= 3:
             for n in comp:
                 nodes.pop(n, None)
+    
+    # ノードを描画
+    for node_id, data in nodes.items():
+        x, y = data['pos']
+        if data['type'] == 0:
+            color = (255, 0, 0)
+        elif data['type'] == 1:
+            color = (0, 0, 255)
+        elif data['type'] == 2:
+            color = (0, 255, 255)
+        elif data['type'] == 3:
+            color = (0, 165, 255)
+        
+        radius = 5 if data['type'] != 3 else 3
+        cv2.circle(marked_img, (x, y), radius, color, -1)
     
     return nodes, edges, marked_img
 
